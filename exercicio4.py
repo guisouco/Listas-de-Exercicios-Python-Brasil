41466a2d5c7b9f6fc7e3f8e5e2ce019cb1587eb8
guisouco <89437136+guisouco@users.noreply.github.com>
2021-09-04 20:53:50 -0300

    estrutura de decisão

---
+++ exercicio4.py
@@ -0,0 +1,17 @@
+"""Faça um Programa que verifique se uma letra digitada é vogal ou consoante."""
+
+letra = str(input('Digite uma letra: ')).upper()
+
+if __name__ == '__main__':
+    if letra == 'A':
+        print('A letra é uma vogal')
+    elif letra == 'E':
+        print('A letra é uma vogal')
+    elif letra == 'I':
+        print('A letra é uma vogal')
+    elif letra == 'O':
+        print('A letra é uma vogal')
+    elif letra == 'U':
+        print('A letra é uma vogal')
+    else:
+        print('A letra é uma consoante')
