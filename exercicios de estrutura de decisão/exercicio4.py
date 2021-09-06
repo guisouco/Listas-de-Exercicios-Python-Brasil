07bb50c94975431e04b8914883d8355d6d0e7ca2
guisouco <guilherme.souzacolacio@gmail.com>
2021-09-06 15:02:34 -0300

    exercicio resolvido

--- exercicios de estrutura de decisão/exercicio4.py
+++ exercicios de estrutura de decisão/exercicio4.py
@@ -2,16 +2,15 @@
 
 letra = str(input('Digite uma letra: ')).upper()
 
-if __name__ == '__main__':
-    if letra == 'A':
-        print('A letra é uma vogal')
-    elif letra == 'E':
-        print('A letra é uma vogal')
-    elif letra == 'I':
-        print('A letra é uma vogal')
-    elif letra == 'O':
-        print('A letra é uma vogal')
-    elif letra == 'U':
-        print('A letra é uma vogal')
-    else:
+if letra == 'A':
+    print('A letra é uma vogal')
+elif letra == 'E':
+    print('A letra é uma vogal')
+elif letra == 'I':
+    print('A letra é uma vogal')
+elif letra == 'O':
+    print('A letra é uma vogal')
+elif letra == 'U':
+    print('A letra é uma vogal')
+else:
         print('A letra é uma consoante')
