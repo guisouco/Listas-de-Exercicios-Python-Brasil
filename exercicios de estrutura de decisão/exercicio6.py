d06830cf300b832224f2cca3085a3a9ebb0ed37e
guisouco <guilherme.souzacolacio@gmail.com>
2021-09-06 14:26:02 -0300

    exercicio 6 resolvido :)

---
+++ exercicios de estrutura de decisão/exercicio6.py
@@ -0,0 +1,13 @@
+"""Faça um Programa que leia três números e mostre o maior deles."""
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite um segundo numero: '))
+n3 = int(input('Digite um terceiro numero: '))
+
+if __name__ == '__main__':
+    if n1>n2 and n1>n3:
+        print(f'O {n1} é maior que {n2} e {n3}')
+    elif n2>n1 and n2>n3:
+        print(f'O {n2} é maior que {n1} e {n3}')
+    elif n3>n1 and n3>n2:
+        print(f'O {n3} é maior que {n2} e {n1}')
