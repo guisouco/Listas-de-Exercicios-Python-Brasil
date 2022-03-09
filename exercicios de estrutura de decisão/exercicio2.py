6bcbc8aaf67910dc57a0cb43fa6b3c43770743b3
guisouco <89437136+guisouco@users.noreply.github.com>
2022-03-09 13:41:07 -0300

    Delete exercicios de estrutura de decisão directory

--- exercicios de estrutura de decisão/exercicio2.py
+++
@@ -1,8 +0,0 @@
-"""Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo"""
-
-v = float(input('Digite um valor: '))
-
-if v>0:
-    print('O valor é positivo.')
-elif v<0:
-    print('O valor é negativo.')
